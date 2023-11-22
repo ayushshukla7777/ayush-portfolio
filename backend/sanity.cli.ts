0fc875dc701dc8e66ceee8fdde1c1b11f8c21cfa
Ayush Aryan <ayushshukla7777@gmail.com>
2023-11-22 14:19:21 +0530

    feat:first commit

---
+++ backend/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'fx8dgu4c',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemas/index.js
@@ -0,0 +1,10 @@
+import about from './abouts';
+import skill from './skills';
+import contact from './contact';
+import experience from './experiences';
+import testimonial from './testimonials';
+import brand from './brands';
+import work from './works';
+import workExperience from './workExperience';
+
+export const schemaTypes = [about, skill, contact, experience, testimonial, brand, work, workExperience]
